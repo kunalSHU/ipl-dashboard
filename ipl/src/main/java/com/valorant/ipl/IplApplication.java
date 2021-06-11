c9cdac31b608f2460811f2fa1472367f4290ef04
kunalSHU <kunalshukla@hotmail.com>
2021-06-11 12:46:55 -0400

    Init Spring BE

---
+++ ipl/src/main/java/com/valorant/ipl/IplApplication.java
@@ -0,0 +1,13 @@
+package com.valorant.ipl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IplApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IplApplication.class, args);
+	}
+
+}
