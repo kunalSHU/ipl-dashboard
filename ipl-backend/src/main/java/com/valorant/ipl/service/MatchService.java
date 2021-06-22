49c12826d2ef3fe8e04217f3fa39ecf6310c45a1
kunalSHU <kunalshukla@hotmail.com>
2021-06-22 16:29:03 -0400

    Added error handling

--- ipl-backend/src/main/java/com/valorant/ipl/service/MatchService.java
+++ ipl-backend/src/main/java/com/valorant/ipl/service/MatchService.java
@@ -1,5 +1,7 @@
 package com.valorant.ipl.service;
 
+import com.valorant.ipl.constants.IplConstants;
+import com.valorant.ipl.exception.TeamNotFoundException;
 import com.valorant.ipl.model.Match;
 import com.valorant.ipl.repository.MatchRepository;
 import lombok.extern.slf4j.Slf4j;
@@ -24,8 +26,12 @@ public class MatchService {
 
         log.info("In get matches list method");
 
-        matchRepository.findByTeam1(teamName).forEach(match -> matchesTeam1.add(match));
-        matchRepository.findByTeam2(teamName).forEach(match -> matchesTeam2.add(match));
+        if (!matchRepository.findByTeam1(teamName).isEmpty()) { // implies team2 is valid too
+            matchRepository.findByTeam1(teamName).forEach(match -> matchesTeam1.add(match));
+            matchRepository.findByTeam2(teamName).forEach(match -> matchesTeam2.add(match));
+        } else {
+            throw new TeamNotFoundException(IplConstants.TEAM_NAME_NOT_FOUND);
+        }
 
         List<Match> allMatches = Stream.concat(matchesTeam1.stream(), matchesTeam2.stream()).collect(Collectors.toList());
 
