c6a9177915a179acf0c36f98ddf32dc536661d52
kunalSHU <kunalshukla@hotmail.com>
2021-06-23 12:37:47 -0400

    Team Page init

---
+++ ipl-frontend/src/pages/TeamPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const TeamPage: React.FC = () => {
+    return (
+        <>
+            <div className="TeamPage">
+                <h1>Team Dashboard</h1>
+            </div>
+        </>
+    )
+}
+
+export default TeamPage;
