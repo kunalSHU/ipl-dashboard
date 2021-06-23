c3510f2239c8ca094d1afd3a3b5d52c80454f2f0
kunalSHU <kunalshukla@hotmail.com>
2021-06-23 13:44:47 -0400

    Added cross origin

--- ipl-backend/src/main/java/com/valorant/ipl/controller/TeamController.java
+++ ipl-backend/src/main/java/com/valorant/ipl/controller/TeamController.java
@@ -7,6 +7,7 @@ import com.valorant.ipl.service.TeamService;
 import lombok.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@CrossOrigin(allowedHeaders = "http://localhost:3000")
 public class TeamController {
 
     @Autowired
