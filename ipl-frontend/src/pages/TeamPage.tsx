65f11fb0dfa20c785358a22b01ce5d30b56370a0
kunalSHU <kunalshukla@hotmail.com>
2021-06-24 09:53:19 -0400

    Added Dropdown

--- ipl-frontend/src/pages/TeamPage.tsx
+++ ipl-frontend/src/pages/TeamPage.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import TeamDropDown from '../components/TeamDropDown'
 
 const TeamPage: React.FC = () => {
     return (
         <>
             <div className="TeamPage">
                 <h1>Team Dashboard</h1>
+                <TeamDropDown/>
             </div>
         </>
     )
