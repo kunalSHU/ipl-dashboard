4204319dfad334b98dcb1ad8e0bbf2f8634c8487
kunalSHU <kunalshukla@hotmail.com>
2021-07-07 15:10:01 -0400

    New component

---
+++ ipl-frontend/src/components/Team.tsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react'
+import { useEffect } from 'react'
+import { useParams } from 'react-router-dom';
+import { getTeamInfo } from '../apis/apiService';
+import history from '../history';
+
+interface ITeam {
+    teamName: string
+}
+
+interface ITeamInfo {
+    id: number
+    teamName: string
+    totalMatches: number
+    totalWins: number
+}
+
+const Team = () => {
+
+    const team : ITeam = useParams();
+    const [teamInfo, setTeamInfo] = useState<ITeamInfo>();
+
+    useEffect(() => {
+        getTeamInfo(decodeURI(team.teamName)).then(res => {
+            setTeamInfo(res.data);
+        })
+
+        return () => {
+            history.go(0)
+        }
+    }, [])
+
+    return (
+        <>
+            <div id="teamInfo">
+                <h1>{teamInfo?.teamName}</h1>
+                <p>Total Wins: {teamInfo?.totalWins}</p>
+                <p>Total Matches: {teamInfo?.totalMatches}</p>
+            </div>
+        </>
+    )
+}
+
+export default Team
