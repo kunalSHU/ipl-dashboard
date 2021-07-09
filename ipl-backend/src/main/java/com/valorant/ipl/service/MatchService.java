65c0a456cb2c1bf2f5fe71647b160c3c6c932ae6
kunalSHU <kunalshukla@hotmail.com>
2021-07-09 16:56:20 -0400

    Remove unused import

--- ipl-backend/src/main/java/com/valorant/ipl/service/MatchService.java
+++ ipl-backend/src/main/java/com/valorant/ipl/service/MatchService.java
@@ -6,7 +6,6 @@ import com.valorant.ipl.model.Match;
 import com.valorant.ipl.repository.MatchRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
