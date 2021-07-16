4ad5dadf1f9d904bc579eee55b3c3d22e0b12aa5
kunalSHU <kunalshukla@hotmail.com>
2021-07-16 13:21:37 -0400

    Security config

--- ipl-backend/src/main/java/com/valorant/ipl/config/SecurityConfig.java
+++ ipl-backend/src/main/java/com/valorant/ipl/config/SecurityConfig.java
@@ -1,2 +1,36 @@
-package com.valorant.ipl.config;public class SecurityConfig {
+package com.valorant.ipl.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@EnableWebSecurity
+@Slf4j
+@CrossOrigin(origins = "http://localhost:3000")
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.cors();
+        http
+                .httpBasic().disable()
+                .formLogin(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeRequests(authorize -> authorize
+                        .antMatchers("/oauth2/**").permitAll()
+                        .anyRequest().authenticated()
+
+                )
+                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
+                .sessionManagement(sessionManagement ->
+                        sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+    }
 }
