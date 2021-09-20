2191d29b0bd05031e30b92e2fe3e6aed86a08809
kunalSHU <kunalshukla@hotmail.com>
2021-09-20 15:09:34 -0400

    Added logs to kibana using logstash

---
+++ ipl-backend/src/main/java/com/valorant/ipl/service/Pet.java
@@ -0,0 +1,10 @@
+package com.valorant.ipl.service;
+
+import java.io.FileReader;
+
+public abstract class MyAbstractClass {
+    public void something() {
+        System.out.println("Hello");
+        FileReader
+    }
+}
