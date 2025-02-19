74fdfbbaadd9ba77bbbeeaa81239e9578577f1ca
kunalSHU <kunalshukla@hotmail.com>
2025-02-19 09:24:18 -0500

    Added url update for k8 cluster and dockerfile update

--- ipl-frontend/src/apis/apiUrl.ts
+++ ipl-frontend/src/apis/apiUrl.ts
@@ -1,4 +1,5 @@
-export const teamsEndpoint = "http://localhost:8081/teams";
-export const teamInfoEndpoint = "http://localhost:8081/team/";
-export const teamMatchesEndpoint = "http://localhost:8081/matches/";
-export const oktaUrlEndpoint = "http://localhost:8081/oauth2/token";
+// change http://localhost:8081 to http://ipl-backend-service:8081
+export const teamsEndpoint = "http://ipl-backend-service:8081/teams";
+export const teamInfoEndpoint = "http://ipl-backend-service:8081/team/";
+export const teamMatchesEndpoint = "http://ipl-backend-service:8081/matches/";
+export const oktaUrlEndpoint = "http://ipl-backend-service:8081/oauth2/token";
