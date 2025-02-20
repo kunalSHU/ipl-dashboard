44d49ae4436f38b8f23ab3cbb349bed357ea561a
kunalSHU <kunalshukla@hotmail.com>
2025-02-20 09:29:52 -0500

    Added change

--- ipl-backend/src/main/java/com/valorant/ipl/config/CorsConfig.java
+++ ipl-backend/src/main/java/com/valorant/ipl/config/CorsConfig.java
@@ -16,7 +16,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://localhost:3000", "http://ipl-frontend-service:8082")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true);
