04898f06550dac8e11c9dd77c03e85993b2b1bcc
kunalSHU <kunalshukla@hotmail.com>
2025-02-26 10:47:08 -0500

    Added change

--- ipl-frontend/src/apis/apiUrl.ts
+++ ipl-frontend/src/apis/apiUrl.ts
@@ -1,5 +1,5 @@
-import { IPL_BACKEND_SERVICE_HOST, LOCALHOST } from "../enum/EnumDeclare";
-export const teamsEndpoint = `http://${window.location.hostname === LOCALHOST ? LOCALHOST : IPL_BACKEND_SERVICE_HOST}:8081/teams`;
-export const teamInfoEndpoint = `http://${window.location.hostname === LOCALHOST ? LOCALHOST : IPL_BACKEND_SERVICE_HOST}:8081/team/`;
-export const teamMatchesEndpoint = `http://${window.location.hostname === LOCALHOST ? LOCALHOST : IPL_BACKEND_SERVICE_HOST}:8081/matches/`;
-export const oktaUrlEndpoint = `http://${window.location.hostname === LOCALHOST ? LOCALHOST : IPL_BACKEND_SERVICE_HOST}:8081/oauth2/token`;
+import { IPL_BACKEND_SERVICE_HOST, LOCALHOST, LOCALHOST_URL } from "../enum/EnumDeclare";
+export const teamsEndpoint = `http://${window.location.hostname === LOCALHOST ? LOCALHOST_URL : IPL_BACKEND_SERVICE_HOST}/api/teams`;
+export const teamInfoEndpoint = `http://${window.location.hostname === LOCALHOST ? LOCALHOST_URL : IPL_BACKEND_SERVICE_HOST}/api/team/`;
+export const teamMatchesEndpoint = `http://${window.location.hostname === LOCALHOST ? LOCALHOST_URL : IPL_BACKEND_SERVICE_HOST}/api/matches/`;
+export const oktaUrlEndpoint = `http://${window.location.hostname === LOCALHOST ? LOCALHOST_URL : IPL_BACKEND_SERVICE_HOST}/api/oauth2/token`;
--- ipl-frontend/src/enum/EnumDeclare.ts
+++ ipl-frontend/src/enum/EnumDeclare.ts
@@ -1,3 +1,4 @@
 export const OKTA_HOST = "https://dev-30002792.okta.com";
+export const LOCALHOST_URL = "localhost:8081";
 export const LOCALHOST = "localhost";
-export const IPL_BACKEND_SERVICE_HOST = "ipl-backend-service";
+export const IPL_BACKEND_SERVICE_HOST = "ipl.local";
